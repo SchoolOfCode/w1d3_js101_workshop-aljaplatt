d6b7b14ef46ed01327dcb06533003a2dd2131e0c
Alex Platt <alja.platt@gmail.com>
2021-11-17 13:50:21 +0000

    type play

---
+++ script.js
@@ -0,0 +1,24 @@
+let numberBox = 7;
+
+console.log(numberBox);
+
+numberBox += 50;
+console.log(numberBox);
+
+numberBox -= 10;
+console.log(numberBox);
+
+numberBox /= 2;
+console.log(numberBox);
+
+let words = "I am a string";
+
+words = words + " more string";
+
+console.log(words);
+console.log(12 * 4);
+
+let x = 9;
+if (12 > x) {
+  console.log("Go 12!");
+}
